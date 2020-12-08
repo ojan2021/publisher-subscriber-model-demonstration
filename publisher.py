8e8ad9827773b300b1b582c44610854813dadb2a
Orhan Jan <orhancan85@gmail.com>
2020-12-08 22:37:14 +0400

    Commented the lines

--- publisher.py
+++ publisher.py
@@ -4,21 +4,19 @@ import sys
 
 print('Please provide a folder name with a full path, for example, C:\Files\docs: ')
 path = input()
-shortPathName = os.path.basename(path) #We shorten the whole directory name to the one folder for the sake of simplicity
+shortPathName = os.path.basename(path) # We shorten the whole directory name to the one folder for the sake of simplicity
 
 context = zmq.Context()
-s = context.socket(zmq.PUB)  # create a publisher socket
-p = 'tcp://' + str(sys.argv[1]) + ':' + str(sys.argv[2])  # how and where to communicate
+s = context.socket(zmq.PUB)  # Create a publisher socket
+p = 'tcp://' + str(sys.argv[1]) + ':' + str(sys.argv[2])  # How and Where to communicate
                                                           # (Program gets IP address and Port number from the user via commandline arguements
-s.bind(p)  # bind socket to the address
+s.bind(p)  # Bind socket to the address
 
 listOfFiles = []
-listOfFiles = os.listdir(path)  #List of all files and folders in the required path
+listOfFiles = os.listdir(path)  # List of all files and folders in the required path
 
 while True:
-    time.sleep(5)  # wait every 5 seconds
+    time.sleep(5)  # Wait every 5 seconds
     for i in listOfFiles:
         s.send_string('Folder ' + shortPathName + ' has ' + str(i))     # Sending list of files/folders to subscriber.
     s.send_string('\n')
-
-
--- subscriber.py
+++ subscriber.py
@@ -2,13 +2,13 @@ import zmq
 import sys
 
 context = zmq.Context()
-s=context.socket(zmq.SUB)  # Note.
-s.setsockopt_string(zmq.SUBSCRIBE, 'Folder')  # Note.
+s = context.socket(zmq.SUB)
+s.setsockopt_string(zmq.SUBSCRIBE, 'Folder')  # We subscribe to the messages that contain Folder word.
 s.subscribe('\n')
 
-p = 'tcp://' + str(sys.argv[1]) + ':' + str(sys.argv[2])  # how and where to communicate
+p = 'tcp://' + str(sys.argv[1]) + ':' + str(sys.argv[2])  # How and Where to communicate
                                                           # (Program gets IP address and Port number from the user via commandline arguements
-s.connect(p)  # connect to the server
+s.connect(p)  # Connect to the publisher
 
 while True:
     files = s.recv_string()     # Receiving the list of files/folders from publisher.
